ce451b55aa4c3a95b6dfd287089a72d0b1c54bcb
YanYan Lee <leeya@flip2.engr.oregonstate.edu>
2019-05-29 10:45:45 -0700

    font page

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 12345;
+
+app.use(express.static(__dirname + '/public'));
+
+// root path to the front page
+app.get('/', function(req, res, next){
+	console.log("root page is loading");
+	res.status(200).sendFile(__dirname+ 'public/index.html');
+});
+
+app.get('*', function(req, res, next){
+	console.log("404 page is loading");
+	res.status(404).sendFile(__dirname+'public/notfound.html');
+});
+
+app.listen(port, function(){
+	console.log(" == server is listening: ", port);
+});
