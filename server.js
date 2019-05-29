ec7b2c44dd191dc1fb48a66fdc0e06749f5b2ac7
YanYan Lee <leeya@flip3.engr.oregonstate.edu>
2019-05-29 16:23:59 -0700

    changes in server.js 05/29

--- server.js
+++ server.js
@@ -9,12 +9,19 @@ app.use(express.static(__dirname + '/public'));
 // root path to the front page
 app.get('/', function(req, res, next){
 	console.log("root page is loading");
-	res.status(200).sendFile(__dirname+ 'public/index.html');
+	res.status(200).sendFile(__dirname+ '/public/index.html');
 });
 
+// home page
+app.get('/home',function(req, res, next){
+	console.log("home page is loading");
+	res.status(200).sendFile(__dirname+ '/public/home.html');
+});
+
+// 404 page
 app.get('*', function(req, res, next){
 	console.log("404 page is loading");
-	res.status(404).sendFile(__dirname+'public/notfound.html');
+	res.status(404).sendFile(__dirname+ '/public/notfound.html');
 });
 
 app.listen(port, function(){
