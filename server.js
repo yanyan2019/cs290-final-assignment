5b206424ead3355e4f4e85810e7671f04eb10c6e
kjiroux <46536292+kjiroux@users.noreply.github.com>
2019-06-06 16:03:14 -0700

    Updated server.js
    Everything that was in there prior is still in there, just commented out. Don't know if functional but should work, maybe.

--- server.js
+++ server.js
@@ -1,27 +1,78 @@
 // server.js
 
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 12345;
+//var express = require('express');
+//var app = express();
+//var port = process.env.PORT || 12345;
 
-app.use(express.static(__dirname + '/public'));
+
+//var tarotData = require('./public/tarot');
+
+//app.use(express.static(__dirname + '/public'));
+
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
+var tarotData = require('./public/tarotData');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+//var mongoHost = process.env.MONGO_HOST;
+//var mongoPort = process.env.MONGO_PORT || 27017;
+//var mongoUser = process.env.MONGO_USER;
+//var mongoPassword = process.env.MONGO_PASSWORD;
+//var mongoDBName = process.env.MONGO_DB_NAME;
+
+//var mongoUrl = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDBName}`;
+//var db = null;
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
 
 // root path to the front page
 app.get('/', function(req, res, next){
 	console.log("root page is loading");
-	res.status(200).sendFile(__dirname+ '/public/index.html');
+    res.status(200).render('homePage'); //where homePage is the templetized version of public/index.html
+    //FIX ME
+    
+    //res.status(200).sendFile(__dirname+ '/public/index.html');
 });
 
 // home page
 app.get('/home',function(req, res, next){
-	console.log("home page is loading");
-	res.status(200).sendFile(__dirname+ '/public/home.html');
+    console.log("home page is loading");
+    //where displayPage = the basic handlebars template page of BEFORE a card has been selected
+    // More than likely FIX ME here
+    res.status(200).render('displayPage');
+	//res.status(200).sendFile(__dirname+ '/public/home.html');
+});
+
+// function to retrive a specific card
+app.get('/tarot/:card', function (req, res, next) {
+    var card = req.params.card.toLowerCase();
+    if (tarotData[card]) {
+        //templatize page's for cards FIX ME 
+        res.status(200).render('tarotPage', tarotData[card]);
+    } else {
+        next();
+    }
 });
 
 // 404 page
 app.get('*', function(req, res, next){
-	console.log("404 page is loading");
-	res.status(404).sendFile(__dirname+ '/public/notfound.html');
+    console.log("404 page is loading");
+
+    res.status(404).render('404page');
+	//res.status(404).sendFile(__dirname+ '/public/notfound.html');
 });
 
 app.listen(port, function(){
