0318e0c2a1eaf8abfdfe671add20310aac7dc20f
rivasty <49377307+rivasty@users.noreply.github.com>
2019-06-06 16:19:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 console.log("=index.js=");
-tc = document.getElementByID('card');
+var tc = document.getElementByID('card');
+var hasAp = false;
 function appendD77() {
   var d77 = document.createElement('p');
   d77.innerHTML = "<!-->\n     _\n ,__(.)< (NYAN!)\n  \\___)\n~~~~~~~~~~~~~~~~~~\n<!-->";
-  tc.appendChild(d77);
+  if(!hasAp) {
+    tc.appendChild(d77);
+    hasAp = true;
+  }
 }
+appendD77();
