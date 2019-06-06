d41e24ebd3cd481edd354dae5f4921334d68bf7d
rivasty <49377307+rivasty@users.noreply.github.com>
2019-06-06 16:16:14 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("=index.js=");
+tc = document.getElementByID('card');
+function appendD77() {
+  var d77 = document.createElement('p');
+  d77.innerHTML = "<!-->\n     _\n ,__(.)< (NYAN!)\n  \\___)\n~~~~~~~~~~~~~~~~~~\n<!-->";
+  tc.appendChild(d77);
+}
