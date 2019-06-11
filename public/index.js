92a58f7452a43c5ad65d135a991e6936f6f608db
Pathwerk <49247599+Pathwerk@users.noreply.github.com>
2019-06-11 12:16:32 -0700

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -30,3 +30,69 @@ function createCard(photoURL, caption, name) {
   console.log("== photoCardHTML:", CardHTML);
   return CardHTML;
 }
+
+function handleModalAcceptClick() {
+
+  var photoURL = document.getElementById('photo-url-input').value.trim();
+  var caption = document.getElementById('photo-caption-input').value.trim();
+  var name = document.getElementById('name-input').value.trim();
+
+  if (!photoURL || !caption || !name) {
+    alert("You must fill in all of the fields!");
+  } else {
+
+    var newCard = createCard(photoURL, caption, name);
+    var CardContainer = document.querySelector('.card-holder');
+    CardContainer.insertAdjacentHTML('beforeend', newCard);
+    hideModal();
+
+  }
+
+}
+
+function showModal() {
+
+  var modal = document.getElementById('add-modal');
+  var modalBackdrop = document.getElementById('modal-backdrop');
+
+  modal.classList.remove('hidden');
+  modalBackdrop.classList.remove('hidden');
+
+}
+
+function clearModalInputs() {
+
+  var modalInputElements = document.querySelectorAll('#add-modal input')
+  for (var i = 0; i < modalInputElements.length; i++) {
+    modalInputElements[i].value = '';
+  }
+
+}
+
+function hideModal() {
+
+  var modal = document.getElementById('add-modal');
+  var modalBackdrop = document.getElementById('modal-backdrop');
+
+  modal.classList.add('hidden');
+  modalBackdrop.classList.add('hidden');
+
+  clearModalInputs();
+
+}
+
+
+window.addEventListener('DOMContentLoaded', function () {
+
+  var addPhotoButton = document.getElementById('add-photo-button');
+  addPhotoButton.addEventListener('click', showModal);
+
+  var modalAcceptButton = document.getElementById('modal-accept');
+  modalAcceptButton.addEventListener('click', handleModalAcceptClick);
+
+  var modalHideButtons = document.getElementsByClassName('modal-hide-button');
+  for (var i = 0; i < modalHideButtons.length; i++) {
+    modalHideButtons[i].addEventListener('click', hideModal);
+  }
+
+});
