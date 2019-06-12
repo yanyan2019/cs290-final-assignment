1afb475b9ba9ab903d1335195751e3e743bc7f6d
rivasty <49377307+rivasty@users.noreply.github.com>
2019-06-11 23:22:17 -0700

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -1,23 +1,41 @@
-var postBox = document.getElementById('posts');
+var postBox = document.getElementById('postBox');
 var form = {
 	"name": document.getElementById('ftitle'),
 	"text": document.getElementById('ftext'),
 	"button": document.getElementById('fbutton')
 }
 function submitForm(event) {
-	console.log({
-		"name": form.name.value,
-		"text":  form.text.value
-	});
+	var postReq = new XMLHttpRequest();
+	var reqUrl = '/comment';
+	postReq.open('POST',reqUrl);
 	if(form.name.value == "" || form.text.value == "") {
 		alert("You did not fill out all fields!");
 		return;
 	}
-	var cardContext = {
-    name: form.name.value,
+	var reqPost = JSON.stringify({
+		name: form.name.value,
 		text: form.text.value
-  };
-  var cardHTML = Handlebars.templates.ccard(cardContext);
-  postBox.insertAdjacentHTML('beforeend',cardHTML);
+	});
+	console.log(postReq);
+	console.log(reqPost);
+	postReq.addEventListener('load',function (event) {
+		console.log("postReq!");
+		if(event.target.status === 200) {
+			var cardContext = {
+    		name: form.name.value,
+				text: form.text.value
+  		};
+			var post = Handlebars.templates.ccard(cardContext);
+  		form.name.value = "";
+  		form.text.value = "";
+  		console.log("post: ",post); 
+  		postBox.insertAdjacentHTML('afterbegin',post);
+		}
+		else {
+			alert("Error creating comment!" + event.target.response);
+		}
+	});
+	postReq.setRequestHeader('Content-Type','application/json');
+	postReq.send(reqPost);
 }
 form.button.addEventListener('click',submitForm);
