241763cbdba541c42ebc676317316ca7b5156c86
rivasty <49377307+rivasty@users.noreply.github.com>
2019-06-11 23:20:59 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,4 @@
 // server.js
-
-//var express = require('express');
-//var app = express();
-//var port = process.env.PORT || 12345;
-
-
-//var tarotData = require('./public/tarot');
-
-//app.use(express.static(__dirname + '/public'));
-
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
@@ -17,13 +7,15 @@ var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 
 var tarotData = require('./public/tarot.json');
-var postData; // SET THIS TO THE MONGODB FOR COMMENTS
-
+var postData = require('/public/posts.json');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 console.log("<!-->\n     _\n ,__(.)< (NYAN!)\n  \\___)\n~~~~~~~~~~~~~~~~~~\n<!-->");
+function pushPostJSON(ntext, ttext) {
+	var jso = fs.open('/public/posts.json');
+}
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
@@ -31,13 +23,11 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname+'/public'));
 
-// root path to the front page
 app.get('/', function(req, res, next){
 	console.log("root page is loading");
   res.status(200).sendFile(__dirname+ '/public/index.html');
 });
 
-// home page
 app.get('/tarot',function(req, res, next){
     console.log("home page is loading");
     //where displayPage = the basic handlebars template page of BEFORE a card has been selected
@@ -45,7 +35,6 @@ app.get('/tarot',function(req, res, next){
     res.status(200).render('tarotPage');
 });
 
-// function to retrive a specific card
 app.get('/tarot/:card', function (req, res, next) {
     var card = req.params.card.toLowerCase();
     if (tarotData[card]) {
@@ -56,20 +45,31 @@ app.get('/tarot/:card', function (req, res, next) {
           name: tarotData[card].name,
           description: tarotData[card].Description,
           photo: tarotData[card].img,
-          
           posts: postData
         });
     } else {
         next();
     }
 });
-
-// 404 page
+app.post('/comment', function(req, res, next) {
+	if(req.body && req.body.name && req.body.text) {
+		console.log("req.body: ",req.body);
+		var post = {
+			name: req.body.name,
+			text: req.body.text
+		};
+		console.log("post: ",post);
+		pushPostJSON(post.name,post.text);
+	}
+	else {
+		req.status(400).send("Request needs body with name and text!");
+	}
+});
 app.get('*', function(req, res, next){
     console.log("404 page is loading");
     res.status(404).render('404');
 });
-
 app.listen(port, function(){
 	console.log(" == server is listening: ", port);
 });
+
